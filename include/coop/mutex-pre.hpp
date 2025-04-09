18ae072f352bde7b66f66f274ceeb4b284979046
mojyack <mojyack@gmail.com>
2025-04-09 14:47:19 +0900

    mutex: optimize to not suspend if not held

--- include/coop/mutex-pre.hpp
+++ include/coop/mutex-pre.hpp
@@ -3,11 +3,22 @@
 #include "multi-event-pre.hpp"
 
 namespace coop {
+struct Mutex;
+
+struct MutexAwaiter {
+    Mutex* mutex;
+
+    auto await_ready() const -> bool;
+    template <CoHandleLike CoHandle>
+    auto await_suspend(CoHandle caller_task) -> void;
+    auto await_resume() const -> void {}
+};
+
 struct Mutex {
     MultiEvent event;
     bool       held = false;
 
-    auto lock() -> coop::Async<void>;
+    auto lock() -> MutexAwaiter;
     auto unlock() -> void;
 };
 } // namespace coop
--- include/coop/mutex.hpp
+++ include/coop/mutex.hpp
@@ -1,14 +1,19 @@
 #pragma once
 #include "multi-event.hpp"
 #include "mutex-pre.hpp"
-#include "promise.hpp"
 
 namespace coop {
-inline auto Mutex::lock() -> coop::Async<void> {
-    if(held) {
-        co_await event;
-    }
-    held = true;
+inline auto MutexAwaiter::await_ready() const -> bool {
+    return !std::exchange(mutex->held, true);
+}
+
+template <CoHandleLike CoHandle>
+inline auto MutexAwaiter::await_suspend(CoHandle caller_task) -> void {
+    mutex->event.await_suspend(std::move(caller_task));
+}
+
+inline auto Mutex::lock() -> MutexAwaiter {
+    return MutexAwaiter(this);
 }
 
 inline auto Mutex::unlock() -> void {
